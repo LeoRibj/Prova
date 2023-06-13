d0ceebd142f2333e377dc580f8d8826cc46f877e
ManigoldoCR <leo_dbz1@hotmail.com>
2023-06-13 19:24:00 -0300

    Primeiro push

--- Classes/Conta.py
+++ Classes/Conta.py
@@ -1,18 +1,19 @@
 from datetime import datetime
 import datetime as dt
 class Conta:
-    def __init__(self,n_conta:str="",dataleitura:datetime=datetime.now,valor:float=0,n_leitura:str="",kwGasto:int=0) -> None:
-        self.__n_conta=n_conta
-        self.__dataleitura=dataleitura
-        self.__valor=valor  
-        self.__data_pgto=data_pgto= dt.timedelta(days=20)+dataleitura
-        self.__kwGasto=kwGasto
-        self.__n_leitura=n_leitura
+    def __init__(self,n_conta:str="",dataleitura:datetime=datetime.now(),nleitura:str="", valor:float=0,kwgasto:int=0) -> None:
+        self.__n_conta = n_conta
+        self.__dataleitura = dataleitura
+        self.__n_leitura=nleitura
+        self.__valor_pagar = valor
+        self.__data_pgto = dt.timedelta(days=20)+dataleitura
+        self.__kwGasto = kwgasto
+    
     def getDataLeitura(self):
-        return self.__dataleitura.strftime(" %d %m %Y")
-    def getkwgasto(self):
+        return self.__dataleitura.strftime(" %d %m %Y ")
+    def getKwGatsto(self):
         return self.__kwGasto
-    def getvalorpagar(self):
-        return self.__valor
+    def getValorPagar(self):
+        return self.__valor_pagar
     def getDadosConta(self):
         pass
